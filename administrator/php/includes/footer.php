32b8e28bed01d0bd35c6fc76384ff3a9fdf1d20f
aragon12 <sm95007@gmail.com>
2020-05-02 20:32:43 +0530

    initial push

---
+++ administrator/php/includes/footer.php
@@ -0,0 +1,5 @@
+	<div id="footer">
+		&copy;2019 Online Crime Report. Dashboard
+	</div>
+	</body>
+</html>
---
+++ php/includes/footer.php
@@ -0,0 +1,9 @@
+	<div id="footer">
+        <p style="margin-top:5px;">Copyright &copy; Online Crime Reporting <a href="#" title="email">Projectworlds </a>  </p>
+		<div style="float:right; margin-top:-35px;margin-right:5px;">
+			<a href="#"><img src="images/facebook_32.png" alt="" /></a>&nbsp;<a href="#"><img src="images/twitter_32.png" alt="" /></a>
+		</div>
+  </div>
+  </div>
+	</body>
+</html>
